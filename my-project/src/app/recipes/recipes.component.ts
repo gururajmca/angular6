37ea4b6345f388aebcdeb34a6ee03a13ed59bbea
Gururaj <gururaj@netskope.com>
2019-02-22 17:36:05 +0530

    Added shopping services

--- my-project/src/app/recipes/recipes.component.ts
+++ my-project/src/app/recipes/recipes.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from './recipe.model';
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers[ RecipeService]
+  providers: [ RecipeService]
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
--- my-project/src/app/shopping-list/shopping-list.service.ts
+++ my-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 export class ShoppingListService {
 
+  onIngredientChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredients(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.onIngredientChanged.emit(this.ingredients.slice());
+  }
 }
