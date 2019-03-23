318eb5cbf2228c21dd04aede99118dfbc472c63b
Gururaj <gururaj@netskope.com>
2019-03-23 13:13:57 +0530

    Added ngForm Module changes

--- my-form-app/src/app/app.component.ts
+++ my-form-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { ngForm } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +7,49 @@ import { ngForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') userForm: ngForm;
-  defaultQuestion = 'pet';
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'teacher';
   answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
-  // onFormSubmit(form: ngForm) {
+
+  // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
 
-  onFormSubmit() {
-    console.log(this.userForm);
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
